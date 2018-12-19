fe681333d29c42c0fa8a4c4c6f32abc8835770b9
Lisa-Marie Dekker <lisamariedekker@outlook.com>
2018-12-19 12:09:16 +0100

    :hankey: commented out an import

--- src/components/CouponActivate.js
+++ src/components/CouponActivate.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Popup from './Popup';
+// import Popup from './Popup';
 
 export default function CouponActivate(props) {
   // console.log(props.coupon + 'props')
